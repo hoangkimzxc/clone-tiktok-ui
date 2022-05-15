cacca3977273c27df8096c24c4d145124791ec50
hoangnk <hoangkimzxc@gmail.com>
2022-05-15 20:33:22 +0700

    61_cau hinh SCSS

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import './App.css';
-import Button from '~/components/Button';
-
 function App() {
-    return (
-        <div>
-            <Button />
-        </div>
-    );
+  return (
+    <div>
+      <h1>React app</h1>
+    </div>
+  );
 }
 
 export default App;
