3b9b43232cdec8b93064053307672576fe63335d
hoangnk <hoangkimzxc@gmail.com>
2022-05-16 15:17:52 +0700

    63_dung khung layout mac dinh

--- src/components/layout/DefaultLayout/index.js
+++ src/components/layout/DefaultLayout/index.js
@@ -1,13 +1,19 @@
 import Header from "~/components/layout/components/Header";
 import Sidebar from "./Sidebar";
+import styles from "./DefaultLayout.module.scss";
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(styles);
 
 function DefaultLayout({ children }) {
   return (
     <>
-      <Header />
-      <div className="container">
-        <Sidebar />
-        <div className="content">{children}</div>
+      <div className={cx("wrapper")}>
+        <Header />
+        <div className={cx("container")}>
+          <Sidebar />
+          <div className={cx("content")}>{children}</div>
+        </div>
       </div>
     </>
   );
