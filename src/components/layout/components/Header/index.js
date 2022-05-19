9c0d45c9a09cebebc9fc38edde1bed1c5c2784c8
hoangnk <hoangkimzxc@gmail.com>
2022-05-19 21:33:43 +0700

    68_xay dung ui header #5

--- src/components/layout/components/Header/index.js
+++ src/components/layout/components/Header/index.js
@@ -24,6 +24,13 @@ const MENU_ITEMS = [
   {
     icon: <FontAwesomeIcon icon={faEarthAsia} />,
     title: "English",
+    children: {
+      title: "Language",
+      data: [
+        { type: "language", code: "en", title: "English" },
+        { type: "language", code: "vi", title: "Tieng Viet" },
+      ],
+    },
   },
   {
     icon: <FontAwesomeIcon icon={faCircleQuestion} />,
@@ -44,6 +51,16 @@ function Header() {
       setSearchResult([]);
     }, 0);
   }, []);
+
+  // Handle logic
+  const handleMenuOnchange = (menuItem) => {
+    switch (menuItem.type) {
+      case "language":
+        // Handle change language
+        break;
+      default:
+    }
+  };
 
   return (
     <header className={cx("wrapper")}>
@@ -84,7 +101,7 @@ function Header() {
           <Button text>Upload</Button>
           <Button primary>Log in</Button>
 
-          <Menu items={MENU_ITEMS}>
+          <Menu items={MENU_ITEMS} onChange={handleMenuOnchange}>
             <button className={cx("more-btn")}>
               <FontAwesomeIcon icon={faEllipsisVertical} />
             </button>
